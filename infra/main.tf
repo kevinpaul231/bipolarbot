7da6fbf9ce6bc27957724026515bd6b8ab3b69ed
kkodiyan <kkodiyan@cisco.com>
2020-10-21 13:46:07 +0530

    bugfixes again

--- infra/main.tf
+++ infra/main.tf
@@ -6,8 +6,9 @@ provider "google" {
 }
 
 resource "google_compute_instance" "default" {
-	for_each = toset(["1","2"])
-	name = "${var.instance["name"]}-${each.key}"
+#	for_each = toset(["1","2"])
+#	name = "${var.instance["name"]}-${each.key}"
+	name = var.instance["name"]
 	machine_type = var.instance["type"]
 	zone = var.instance_location["zone"]
 
@@ -27,15 +28,22 @@ resource "google_compute_instance" "default" {
 		ssh-keys = "${var.ssh_key_pub["user"]}:${file(var.ssh_key_pub["key"])}"
 	}
 
+	connection {
+		host = self.network_interface[0].access_config[0].nat_ip
+		user = var.ssh_key_pub["user"]
+		private_key = file("~/.ssh/id_rsa")
+		timeout = "500s"
+	}
+
 	provisioner "remote-exec" {
 		script = "../prep.sh"
 
-		connection {
-			#TODO Move private key and user details to var.tf, call it here
-			host = "self.network_interface[0].access_config[0].nat_ip"
-			user = var.ssh_key_pub["user"]
-			private_key = file("~/.ssh/id_rsa")	
-		}
+#		connection {
+#			#TODO Move private key and user details to var.tf, call it here
+#			host = self.network_interface.0.access_config.0.nat_ip
+#			user = var.ssh_key_pub["user"]
+#			private_key = file("~/.ssh/id_rsa")	
+#		}
 	}
 
 }
@@ -48,4 +56,5 @@ resource "google_compute_firewall" "default" {
 		protocol = "tcp"
 		ports = ["22","8000"]
 	}
+	source_ranges = ["0.0.0.0/0"]
 }
--- prep.sh
+++ prep.sh
@@ -1,7 +1,7 @@
 sudo apt-get update
 sudo apt-get -y install build-essential python3-pip
 cd /
-git clone https://github.com/kevinpaul231/bipolarbot.git
+sudo git clone https://github.com/kevinpaul231/bipolarbot.git
 cd bipolarbot
 #TODO Move to python venv 
 sudo pip3 install -r app/requirements.txt
